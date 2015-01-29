44aa328480454f843bcec7a0592573e2ee3051f9
Ivancha-Igor <ivan4afamily@gmail.com>
2015-01-29 10:45:38 +0200

    minor changes in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates_uniqueness_of :email
+  validates :password,
+            length: { minimum: 6 }
+  validates :email,
+            presence: true,
+            uniqueness: true
+  validates :name,
+            presence: true,
+            length: { maximum: 50 }
 end
