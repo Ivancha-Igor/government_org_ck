005c243259b2071eb52349879b419db5551a054c
Ivancha-Igor <ivan4afamily@gmail.com>
2015-01-29 12:45:39 +0200

    strong user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :email, :password))
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
     else
       redirect_to :back
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
