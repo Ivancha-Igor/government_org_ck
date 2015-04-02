3be15b8fba28445ff655da01c20705628b4291d9
Ivancha-Igor <ivan4afamily@gmail.com>
2015-04-02 17:09:08 +0300

    Fix controller for omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,15 +14,9 @@ class SessionsController < ApplicationController
 
   def create_session
     auth = request.env['omniauth.auth']
-    if User.where(email: auth.info.email.downcase).present?
-      respond_to do |format|
-        format.html { redirect_to :back, notice: t('messages.user_with_that_email_exist') }
-      end
-    else
-      user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
-      session[:user_id] = user.id
-      redirect_to root_url
-    end
+    user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url
   end
 
   def logout
