4d76acdecddddd0404749fed89c9ca576c1a928e
Ivancha-Igor <ivan4afamily@gmail.com>
2015-04-17 21:33:19 +0300

    Refactor organization controller and views

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -25,7 +25,8 @@ class OrganizationsController < ApplicationController
 
     respond_to do |format|
       if @organization.save
-        format.html { redirect_to @organization, notice: t('messages.add_org') }
+        flash[:success] = t('messages.add_org')
+        format.html { redirect_to @organization }
         format.json { render :show, status: :created, location: @organization }
       else
         format.html { render :new }
@@ -37,7 +38,8 @@ class OrganizationsController < ApplicationController
   def update
     respond_to do |format|
       if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: t('messages.edit_org') }
+        flash[:success] = t('messages.edit_org')
+        format.html { redirect_to @organization }
         format.json { render :show, status: :ok, location: @organization }
       else
         format.html { render :edit }
@@ -49,7 +51,8 @@ class OrganizationsController < ApplicationController
   def destroy
     @organization.destroy
     respond_to do |format|
-      format.html { redirect_to organizations_url, notice: t('messages.delete_org') }
+      flash[:success] = t('messages.delete_org')
+      format.html { redirect_to organizations_url }
       format.json { head :no_content }
     end
   end
